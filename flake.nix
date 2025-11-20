475e2e9a402eee8171376eeb588178f71f1dccd8
sw1310 <rwx-github@bytz.me>
2025-11-19 19:17:48 -0800

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+# /mnt/etc/nixos/flake.nix
+{
+  inputs = {
+    # NOTE: Replace "nixos-25.05" with that which is in system.stateVersion of
+    # configuration.nix. You can also use latter versions if you wish to
+    # upgrade.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    # NOTE: 'nixos' is the default hostname set by the installer.  Change to hostname specified in your configuration.nix file
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      # NOTE: Change this to aarch64-linux if you are on ARM
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+      inputs.disko.nixosModules.disko
+    };
+  };
+}
